3aa14d31342a35c36fff4c9c0eeef77f286de155
XYZ12890 <x3134089@gmail.com>
2025-07-18 12:25:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,5 +43,21 @@ async def on_message(message):
     await bot.process_commands(message)
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+    keep_alive()
 bot.run(TOKEN)
   
